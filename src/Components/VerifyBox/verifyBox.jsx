b4e7e7ecf9d76b508c1c566a1ef924b090dc802d
Ritam Biswas <ritambiswas1801@gmail.com>
2024-03-14 15:56:46 +0530

    updating flies

--- src/Components/VerifyBox/verifyBox.jsx
+++ src/Components/VerifyBox/verifyBox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import 'regenerator-runtime';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
